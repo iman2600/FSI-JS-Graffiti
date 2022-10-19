400a1d3d9e6a18f0f08a82392aed96c0c27aa4b4
Isaac <iman2600@gmail.com>
2022-10-18 22:05:45 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-
 // Select some elements...
+
+let header = document.querySelector("#page-header");
+header.style.textAlign = "left";
+
+let dogNames = document.querySelectorAll(".dog-name");
+for (let i = 0; i < dogNames.length; i++) {
+  dogNames[i].style.textAlign = "left";
+}
+
+let dogImages = document.querySelectorAll(".dog-image");
+for (let i = 0; i < dogImages.length; i++) {
+  dogImages[i].style.borderRadius = "50px";
+}
